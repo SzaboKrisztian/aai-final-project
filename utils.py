321f24dae9836c4bba7799e8c4d4abd179d6cdac
Cristina Doroftei <dorofteicristina19@gmail.com>
2021-11-27 11:48:43 +0100

    added comments

--- utils.py
+++ utils.py
@@ -10,28 +10,38 @@ banned_cats = [
     'squiggle', 'line', 'circle', 'yoga'
 ]
 
+#rezolutie de 256 pe 256 
+#alb-negru e si el de la 0 la 256
+#magnifiation is smoothing the lines
+#with magnification 4, the drawing will be drawn in the memory 4 times bigger than the resolution, and when it's rendered, it will be drawn 4 times smaller by using the antialias, and that makes the image smoother
 def render_single(data, resolution=256, magnification=4, invert_color=False, stroke_width_scale=1):
     scale_factor = 256 / resolution
     color_bg = 255 if not invert_color else 0
     color_fg = 0 if not invert_color else 255
     res = resolution * magnification
+    #open a new image in memory
     image = Image.new('L', (res, res), color_bg)
     ctx = ImageDraw.Draw(image)
+    #find the max x and max y in order to center the image
     xs = list(map(lambda stroke: stroke[0], data))
     ys = list(map(lambda stroke: stroke[1], data))
     max_x = max(chain(*xs))
     max_y = max(chain(*ys))
     add_x = lambda n: ((n * magnification / scale_factor) + (((res - 1) - (max_x * magnification / scale_factor)) // 2))
     add_y = lambda n: ((n * magnification / scale_factor) + (((res - 1) - (max_y * magnification / scale_factor)) // 2))
+    #draw
     for stroke in data:
         ctx.line(list(zip(map(add_x, stroke[0]), map(add_y, stroke[1]))),
             fill=color_fg, width=int(magnification * stroke_width_scale))
+    #smooth
     image = image.resize((res // magnification, res // magnification),
         resample=Image.ANTIALIAS)
+    #create a byte array
     result = io.BytesIO()
     image.save(result, format='png')
     return result.getvalue()
 
+#iterate through all the images and 
 def render_multiple(drawings, resolution=256, magnification=4, invert_color=False, stroke_width_scale=1):
     scale_factor = 256 / resolution
     color_bg = 255
@@ -53,16 +63,20 @@ def render_multiple(drawings, resolution=256, magnification=4, invert_color=Fals
         images.append(image)
     target = Image.new('L', (res, res), color_bg)
     for image in images:
+        #blend the images together
         target = ImageChops.multiply(target, image)
     out = target.resize((resolution, resolution),
             resample=Image.ANTIALIAS)
+    #if you want it inverted, do the pictures white on black
     out = ImageOps.grayscale(out) if not invert_color else ImageChops.invert(ImageOps.grayscale(out))
     result = io.BytesIO()
     out.save(result, format='png')
     return result.getvalue()
 
+
 def get_pixel_data(drawing, resolution=256, magnification=4, invert_color=False, stroke_width_scale=1):
     image = render_single(drawing, resolution, magnification, invert_color, stroke_width_scale)
+    #get an array unidimensional with each pixel having a value between 0 and 1
     return list(map(lambda x: x / 255, list(Image.open(io.BytesIO(image)).getdata())))
 
 def get_dataset_files():
@@ -70,6 +84,7 @@ def get_dataset_files():
     return list(map(lambda f: os.path.join(root, f),
         filter(lambda f: f.endswith('.ndjson') and f.split('.')[0] not in banned_cats, files)))
 
+#open a file line by line and extract the first n entries
 def extract_first_entries(file, size=None, recognized=None):
     with open(file, 'r', encoding='utf8') as f:
         line = f.readline()
@@ -81,6 +96,8 @@ def extract_first_entries(file, size=None, recognized=None):
             line = f.readline()
     return result
 
+#load a whole file and get random entries
+#works less performant
 def extract_random_entries(file, size=None, recognized=None):
     """ recognized can be None, True, or False """
     file_data = [*map(uj.loads, open(file, encoding='utf8'))]
@@ -92,11 +109,13 @@ def extract_random_entries(file, size=None, recognized=None):
     indexes = all_indexes[:size]
     return [file_data[i] for i in indexes]
 
+#flatten the pixels data into pixel0, pixel1, pixel3, etc
 def generate_pixel_columns(df, resolution=256, magnification=4, invert_color=False, stroke_width_scale=1):
     df['pixels'] = df.apply(lambda row: get_pixel_data(row['drawing'], resolution, magnification, invert_color, stroke_width_scale), axis=1)
     split_df = df.pixels.apply(pd.Series).add_prefix('pixel')
     return pd.concat([df.drop(columns=['pixels']), split_df], axis=1)
 
+#try to load a run by id and return a dictionary which will have a key for data, model, pca and scaler
 def load_run(number, verbose=False):
     print(os.getcwd())
     path = os.path.join(os.getcwd(), 'runs', str(number))
@@ -135,3 +154,4 @@ def load_run(number, verbose=False):
             if verbose:
                 print("Warning: either the PCA or scaler failed to load. Very likely this run is corrupt")
     return result
+
